bcce758950f46b3b3d888910cb760bb6cdac4e75
chandraputta <32965755+chandraputta@users.noreply.github.com>
2017-12-04 10:36:34 -0600

    Delete APITest.py

--- APITest.py
+++
@@ -1,7 +0,0 @@
-import requests
-#data = requests.get("https://www.googleapis.com/blogger/v3/blogs/3869481774856968139/posts/?key=AIzaSyA0UzzGOdG0wOWv7PSQZsrfeJQp4lVL2vk").json()
-#print(data);
-
-datawithpostid = requests.get("https://www.googleapis.com/blogger/v3/blogs/3869481774856968139/posts/2537509309351731107?key=AIzaSyA0UzzGOdG0wOWv7PSQZsrfeJQp4lVL2vk").json()
-print(datawithpostid);
-
